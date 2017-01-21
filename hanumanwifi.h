55e9cc583aca7a91c58085d3ef6debc1eb4502cc
ปลิวตามลม <pandachamp@me.com>
2017-01-21 17:26:29 +0700

    initial commit

---
+++ hanumanwifi.h
@@ -0,0 +1,36 @@
+/*
+*
+***Long Ka - IOT Project
+*
+*** Wifi Connector for ESP8266
+** Created by Hanuman
+**/
+
+
+#ifndef HanumanWifiManager
+
+#define HanumanWifiManager
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <memory>
+
+class WifiParameter{
+public:
+
+private:
+    friend class HanumanWifiConnector;
+};
+
+class HanumanWifiConnector {
+public:
+    HanumanWifiConnector(){
+
+    }
+
+private:
+
+};
+
+#endif
